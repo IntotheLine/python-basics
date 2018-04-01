d1e9643821bf00e744d4a553d9de3c5e937360e7
Jonas <34289537+IntotheLine@users.noreply.github.com>
2018-04-02 01:22:12 +0200

    Add files via upload

---
+++ math.py
@@ -0,0 +1,13 @@
+total_dollars = 200
+x_rate = 0.65
+fee = 2
+
+total_pounds = total_dollars / x_rate - fee
+
+print (total_pounds)
+
+import math
+
+print (math.floor(total_dollars))
+
+import pygame
