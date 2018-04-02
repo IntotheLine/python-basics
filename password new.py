7a5a62a98bb78c20960e6e8081930a2e0df7792c
Jonas <34289537+IntotheLine@users.noreply.github.com>
2018-04-03 01:19:13 +0200

    hack attempt of password loop
    infinite loop until the correct password "world" is entered

---
+++ password new.py
@@ -0,0 +1,14 @@
+# passsword new.py
+print("Hello World")
+print("please enter password")
+password = "world"
+user_password = input()
+while password != user_password:
+    continue
+    print("please enter password")
+    user_password = input()
+    if password == user_password:
+        break
+print("welcome")
+
+#password loop until you enter the correct one, next attempt with an counter of 3
